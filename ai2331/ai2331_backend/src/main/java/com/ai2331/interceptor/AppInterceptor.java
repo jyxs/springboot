a40898b7992d791296cd034450b4e492e566c41b
jyxs <56882658@qq.com>
2020-09-22 22:42:20 +0800

    删除相应拦截器
    应用拦截器中增加白名单过滤

--- ai2331/ai2331_backend/src/main/java/com/ai2331/interceptor/AppInterceptor.java
+++ ai2331/ai2331_backend/src/main/java/com/ai2331/interceptor/AppInterceptor.java
@@ -1,5 +1,8 @@
 package com.ai2331.interceptor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,21 +24,61 @@ import com.ai2331.util.WebUtil;
 public class AppInterceptor implements HandlerInterceptor {
 	private Logger logger = LoggerFactory.getLogger(AppInterceptor.class);
 
+	// 白名单
+	private static final Set<String> WHITE_LIST = new HashSet<String>();
+	{
+		WHITE_LIST.add("/css/**");
+		WHITE_LIST.add("/images/**");
+		WHITE_LIST.add("/js/**");
+		WHITE_LIST.add("/fonts/**");
+		WHITE_LIST.add("/login");
+		WHITE_LIST.add("/error/400");
+		WHITE_LIST.add("/error/403");
+		WHITE_LIST.add("/error/500");
+		// swagger
+		WHITE_LIST.add("/doc.html");
+		WHITE_LIST.add("/doc.html#/**");
+		WHITE_LIST.add("/swagger-ui/**");
+		WHITE_LIST.add("/swagger-resources/**");
+		WHITE_LIST.add("/v2/api-docs");
+		WHITE_LIST.add("/v3/api-docs");
+		WHITE_LIST.add("/webjars/**");
+		WHITE_LIST.add("/swagger-ui.html");
+
+		WHITE_LIST.add("/t1/thymleaftest");
+		WHITE_LIST.add("/util/**");
+		WHITE_LIST.add("/t2/thymleaftest");
+	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		logger.debug(">>>>>>>>>>>>>>>>>preHandle");
-		if (request.getRequestURI().contentEquals("/login")) {
-			return true;
-		}
-		Cookie[] cookies = request.getCookies();
-		String token = "";
-		if (null != cookies) {
-			for (Cookie cookie : cookies) {
-				if ("ai2331_token".equals(cookie.getName())) {
-					token = cookie.getValue();
+		String uri = request.getRequestURI();
+		for (String s : WHITE_LIST) {
+			if (s.endsWith("/**")) {
+				if (uri.startsWith(s.substring(0, s.lastIndexOf("/**")))) {
+					return true;
+				}
+			} else {
+				if (s.contentEquals(uri)) {
+					return true;
 				}
 			}
 		}
+//		if (request.getRequestURI().contentEquals("/login")) {
+//			return true;
+//		}
+//		Cookie[] cookies = request.getCookies();
+//		String token = "";
+//		if (null != cookies) {
+//			for (Cookie cookie : cookies) {
+//				if ("ai2331_token".equals(cookie.getName())) {
+//					token = cookie.getValue();
+//				}
+//			}
+//		}
+		//header中需要将登陆成功的token设置到key为ai2331_token的变量中
+		String token = request.getHeader("ai2331_token");
 		if (StringUtils.isEmpty(token)) {
 			throw new UnauthorizedException("token失效，请重新登录");
 		}
